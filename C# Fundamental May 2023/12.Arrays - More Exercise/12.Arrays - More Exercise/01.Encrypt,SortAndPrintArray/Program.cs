0396adf301f335eb5832d149e059758f542d71a8
Miroslav Pavlov <dreb4oz@abv.bg>
2023-06-03 07:59:19 +0300

    added Arrays More Exercise
    added Arrays More Exercise Project 1 and 2

---
+++ C# Fundamental May 2023/12.Arrays - More Exercise/12.Arrays - More Exercise/01.Encrypt,SortAndPrintArray/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01.Encrypt_SortAndPrintArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+            int codeSum = 0;
+            int[] inputSort = new int[inputCount];
+            for (int i = 0; i < inputCount; i++)
+            {
+                string input = Console.ReadLine();
+                codeSum = 0;
+                char[] inputChar = new char[input.Length];
+                for (int k = 0; k < input.Length; k++)
+                {
+                    inputChar[k] = input[k];
+                }
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (inputChar[j] == 'A' || inputChar[j] == 'a' || inputChar[j] == 'E' || inputChar[j] == 'e' || inputChar[j] == 'I' || inputChar[j] == 'i' || inputChar[j] == 'O' || inputChar[j] == 'o' || inputChar[j] == 'U' || inputChar[j] == 'u')
+                    {
+                        int vowelCode = inputChar[j] * input.Length;
+                        codeSum += vowelCode;
+                    }
+                    else
+                    {
+                        int consonantCode = inputChar[j] / input.Length;
+                        codeSum += consonantCode;
+                    }
+                }
+                inputSort[i] = codeSum;
+            }
+            Array.Sort(inputSort);
+            for (int i = 0; i < inputSort.Length; i++)
+            {
+                Console.WriteLine(inputSort[i]);
+            }
+        }
+    }
+}
---
+++ C# Fundamental May 2023/12.Arrays - More Exercise/12.Arrays - More Exercise/02.PascalTriangle/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[number];
+            int[] secondArray = new int[number];
+            int row = 1;
+
+            while (row <= number)
+            {
+                Console.Write(1 + " ");
+                for (int i = 1; i < row; i++)
+                {
+                    if (i == 1)
+                    {
+                    secondArray[i] = firstArray[i] + firstArray[i - 1] + 1;
+                    Console.Write(secondArray[i] + " ");
+                    }
+                    else
+                    {
+                        secondArray[i] = firstArray[i] + firstArray[i - 1];
+                        Console.Write(secondArray[i] + " ");
+                    }
+
+                }
+                for (int j = 1; j < row; j++)
+                {
+                    firstArray[j] = secondArray[j];
+                }
+                row++;
+                Console.WriteLine();
+            }
+        }
+    }
+}

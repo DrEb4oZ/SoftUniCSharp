a325c4553c09327b68474aac63f372770167c0a5
Miroslav Pavlov <dreb4oz@abv.bg>
2023-06-10 23:37:26 +0300

    Added List Exercise
    Added Projects in List Exercise

---
+++ C# Fundamental May 2023/18.Lists-Exercise/18.Lists-Exercise/01.Train/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int wagonCapacity = int.Parse(Console.ReadLine());
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] currentCommand = command.Split();
+                if (currentCommand[0] == "Add")
+                {
+                    int numberOfpassangers = int.Parse(currentCommand[1]);
+                    wagons.Add(numberOfpassangers);
+                }
+
+                else
+                {
+                    int numberOfpassangers = int.Parse(currentCommand[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                    if ((wagonCapacity - wagons[i]) >= numberOfpassangers)
+                        {
+                            wagons[i] += numberOfpassangers;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
---
+++ C# Fundamental May 2023/18.Lists-Exercise/18.Lists-Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integersInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] currentCommand = command.Split();
+                if (currentCommand[0] == "Delete")
+                {
+                    int element = int.Parse(currentCommand[1]);
+                    DeleteAllElementsFromList(integersInput, element);
+                }
+
+                else if (currentCommand[0] == "Insert")
+                {
+                    int element = int.Parse(currentCommand[1]);
+                    int index = int.Parse(currentCommand[2]);
+                    InsertElementAtIndex(integersInput, element, index);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", integersInput));
+        }
+
+        static List<int> DeleteAllElementsFromList(List<int> integersInput, int element)
+        {
+            integersInput.RemoveAll(n => n == element);
+            return integersInput;
+        }
+
+        static List<int> InsertElementAtIndex(List<int> integersInput, int element, int index)
+        {
+            integersInput.Insert(index, element);
+            return integersInput;
+        }
+    }
+}

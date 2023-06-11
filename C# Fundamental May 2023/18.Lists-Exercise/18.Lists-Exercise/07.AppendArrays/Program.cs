65d9b073d2f8af5a81334d7c4a2afcda281cb61c
Miroslav Pavlov <dreb4oz@abv.bg>
2023-06-11 18:23:41 +0300

    update List Exercise
    added new Projects to List Exercise

---
+++ C# Fundamental May 2023/18.Lists-Exercise/18.Lists-Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> integersList = Console.ReadLine()
+                .Split("|")
+                .ToList();
+            string tempString = "";
+            for (int i = 0; i < integersList.Count; i++)
+            {
+                tempString += integersList[integersList.Count - i - 1] + " ";
+            }
+
+            List<int> resultList = tempString
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}

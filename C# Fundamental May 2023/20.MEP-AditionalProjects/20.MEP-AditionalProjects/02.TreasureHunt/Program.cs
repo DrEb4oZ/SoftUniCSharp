d424005a9fa3b2b6ac1b2e4d79e6936e50229140
Miroslav Pavlov <dreb4oz@abv.bg>
2023-06-17 13:14:31 +0300

    Mid Exam Preparation
    added Projects to Mid Exam Preparation

---
+++ C# Fundamental May 2023/20.MEP-AditionalProjects/20.MEP-AditionalProjects/02.TreasureHunt/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+
+namespace _02.TreasureHunt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> treasureChest = Console.ReadLine()
+                .Split("|")
+                .ToList();
+            string action;
+            while ((action = Console.ReadLine()) != "Yohoho!")
+            {
+                string[] currentAction = action
+                    .Split()
+                    .ToArray();
+                if (currentAction[0] == "Loot")
+                {
+                    for (int i = 1; i < currentAction.Length; i++)
+                    {
+                        if (!treasureChest.Contains(currentAction[i]))
+                        {
+                            treasureChest.Insert(0, currentAction[i]);
+                        }
+                    }
+                }
+
+                else if (currentAction[0] == "Drop")
+                {
+                    int index = int.Parse(currentAction[1]);
+                    if (index >= 0 && index < treasureChest.Count)
+                    {
+                        treasureChest.Add(treasureChest[index]);
+                        treasureChest.RemoveAt(index);
+                    }
+                }
+
+                else if (currentAction[0] == "Steal")
+                {
+                    int count = int.Parse(currentAction[1]);
+                    List<string> stolenItems = new List<string>();
+                    if (count > treasureChest.Count)
+                    {
+                        for (int i = 0; i < treasureChest.Count; i++)
+                        {
+                            stolenItems.Add(treasureChest[i]);
+                        }
+
+                        treasureChest.RemoveRange(0, treasureChest.Count);
+                        Console.WriteLine(string.Join(", ", stolenItems));
+                    }
+
+                    else
+                    {
+                        for (int i = treasureChest.Count - count; i < treasureChest.Count; i++)
+                        {
+                            stolenItems.Add(treasureChest[i]);
+                        }
+
+                        treasureChest.RemoveRange(treasureChest.Count - count, count);
+                        Console.WriteLine(string.Join(", ", stolenItems));
+                    }
+                }
+            }
+
+            int sumLengthOfItemsInChest = 0;
+            foreach (string item in treasureChest)
+            {
+                sumLengthOfItemsInChest += item.Length;
+            }
+
+            if (treasureChest.Count > 0)
+            {
+                double averageGain = (double)sumLengthOfItemsInChest / treasureChest.Count;
+                Console.WriteLine($"Average treasure gain: {averageGain:f2} pirate credits.");
+            }
+            else
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+        }
+    }
+}
---
+++ C# Fundamental May 2023/20.MEP-AditionalProjects/20.MEP-AditionalProjects/03.ManOWar/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Transactions;
+
+namespace _03.ManOWar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> pirateShipStatus = Console.ReadLine()
+                .Split(">")
+                .Select(int.Parse)
+                .ToList();
+            List<int> warshipStatus = Console.ReadLine()
+                .Split(">")
+                .Select(int.Parse)
+                .ToList();
+            int maxHealthCapacity = int.Parse(Console.ReadLine());
+            string action;
+            while ((action = Console.ReadLine()) != "Retire")
+            {
+                string[] currentAction = action
+                    .Split();
+                if (currentAction[0] == "Fire")
+                {
+                    int index = int.Parse(currentAction[1]);
+                    int damage = int.Parse(currentAction[2]);
+                    if (index >= 0 && index < warshipStatus.Count)
+                    {
+                        warshipStatus[index] -= damage;
+                        if (warshipStatus[index] <= 0)
+                        {
+                            Console.WriteLine("You won! The enemy ship has sunken.");
+                            return;
+                        }
+                    }
+                }
+
+                else if (currentAction[0] == "Defend")
+                {
+                    int startIndex = int.Parse(currentAction[1]);
+                    int endIndex = int.Parse(currentAction[2]);
+                    int damage = int.Parse(currentAction[3]);
+                    if (startIndex >= 0 && startIndex < pirateShipStatus.Count && endIndex >= 0 && endIndex < pirateShipStatus.Count)
+                    {
+                        for (int i = startIndex; i <= endIndex; i++)
+                        {
+                            pirateShipStatus[i] -= damage;
+                            if (pirateShipStatus[i] <= 0)
+                            {
+                                Console.WriteLine("You lost! The pirate ship has sunken.");
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                else if (currentAction[0] == "Repair")
+                {
+                    int index = int.Parse(currentAction[1]);
+                    int health = int.Parse(currentAction[2]);
+                    if (index >= 0 && index < pirateShipStatus.Count)
+                    {
+                        pirateShipStatus[index] += health;
+                        if (pirateShipStatus[index] > maxHealthCapacity)
+                        {
+                            pirateShipStatus[index] = maxHealthCapacity;
+                        }
+                    }
+                }
+
+                else if (currentAction[0] == "Status")
+                {
+                    int sectionsNeedRepair = 0;
+                    for (int i = 0; i < pirateShipStatus.Count; i++)
+                    {
+                        double currentSectionPercentOfCapacity = (double)pirateShipStatus[i] / maxHealthCapacity;
+                        if (currentSectionPercentOfCapacity < 0.20)
+                        {
+                            sectionsNeedRepair++;
+                        }
+                    }
+
+                    Console.WriteLine($"{sectionsNeedRepair} sections need repair.");
+                }
+            }
+            int sumPirateShipSections = 0;
+            int sumWarshipSections = 0;
+
+            foreach (int section in pirateShipStatus)
+            {
+                sumPirateShipSections += section;
+            }
+
+            foreach (int section in warshipStatus)
+            {
+                sumWarshipSections += section;
+            }
+
+            Console.WriteLine($"Pirate ship status: {sumPirateShipSections}\nWarship status: {sumWarshipSections} ");
+        }
+    }
+}

1aeb0fbb54ff71d7f3fa5c19e27f88040792793e
Miroslav Pavlov <dreb4oz@abv.bg>
2023-07-04 22:05:01 +0300

    Associative Arrays Exercise
    added new projects to associative arrays exercise

---
+++ C# Fundamental May 2023/26.AssociativeArrays-Exercise/26.AssociativeArrays-Exercise/04.SoftUniParking/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _04.SoftUniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int commandCount = int.Parse(Console.ReadLine());
+            Dictionary<string, string> parkingLotUsers = new Dictionary<string, string>();
+            for (int i = 0; i < commandCount; i++)
+            {
+                string[] currentInput = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+                string command = currentInput[0];
+                string name = currentInput[1];
+                if (command == "register")
+                {
+                    string licensePlateNumber = currentInput[2];
+                    if (parkingLotUsers.ContainsKey(name))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+
+                    else
+                    {
+                        parkingLotUsers.Add(name, licensePlateNumber);
+                        Console.WriteLine($"{name} registered {licensePlateNumber} successfully");
+                    }
+                }
+
+                else if (command == "unregister")
+                {
+                    if (!parkingLotUsers.ContainsKey(name))
+                    {
+                        Console.WriteLine($"ERROR: user {name} not found");
+                    }
+
+                    else
+                    {
+                        parkingLotUsers.Remove(name);
+                        Console.WriteLine($"{name} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var kvp in parkingLotUsers)
+            {
+                Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ C# Fundamental May 2023/26.AssociativeArrays-Exercise/26.AssociativeArrays-Exercise/05.Courses/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05.Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] currentStundet = command
+                    .Split(" : ")
+                    .ToArray();
+                string courseName = currentStundet[0];
+                string studentName = currentStundet[1];
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName, new List<string>());
+                }
+                courses[courseName].Add(studentName);
+            }
+
+            foreach (var kvp in courses)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}");
+                foreach (var student in kvp.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}

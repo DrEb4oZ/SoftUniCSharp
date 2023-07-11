fec2869c83462084ce20388efb8513f02172c18a
Miroslav Pavlov <dreb4oz@abv.bg>
2023-07-12 00:12:54 +0300

    Text Processing Exercise
    added projects to text processing exercise

---
+++ C# Fundamental May 2023/29.TextProcessing-Exercise/29.TextProcessing-Exercise/01.ValidUsernames/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNames = Console.ReadLine()
+                .Split(", ");
+            foreach (var user in userNames)
+            {
+                if (user.Length >= 3 && user.Length <= 16 && user.All(x => char.IsDigit(x) || char.IsLetter(x) || x == '_' || x == '-'))
+                {
+                    Console.WriteLine(user);
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamental May 2023/29.TextProcessing-Exercise/29.TextProcessing-Exercise/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _02.CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split();
+            int result = StringCharsMultiply(input[0], input[1]);
+            Console.WriteLine(result);
+
+        }
+        private static int StringCharsMultiply(string firstString, string secondString)
+        {
+            int sum = 0;
+            int length = Math.Min(firstString.Length, secondString.Length);
+            for (int i = 0; i < length; i++)
+            {
+                sum += firstString[i] * secondString[i];
+            }
+
+            sum += SumOfExcesiveChars(firstString, secondString);
+            return sum;
+        }
+        private static int SumOfExcesiveChars(string firstString, string secondString)
+        {
+            int sum = 0;
+            if (firstString.Length > secondString.Length)
+            {
+                for (int i = secondString.Length; i < firstString.Length; i++)
+                {
+                    sum += firstString[i];
+                }
+            }
+
+            else
+            {
+                for (int i = firstString.Length; i < secondString.Length; i++)
+                {
+                    sum += secondString[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}

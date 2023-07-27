7ab23b5afca1aebe4881a0099d364c193abb571c
Miroslav Pavlov <dreb4oz@abv.bg>
2023-07-27 20:52:54 +0300

    Regular Exam Preparation
    added new projects to regular exam preparation

---
+++ C# Fundamental May 2023/35.REP-AditionalProjects/35.REP-AditionalProjects/02.DestinationMapper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string markedLocations = Console.ReadLine();
+            string filterMarkedLocations = @"([=|/])(?<validLocation>[A-Z][A-Za-z]{2,})\1";
+            MatchCollection markedLocatonsCollection = Regex.Matches(markedLocations, filterMarkedLocations);
+            int travelPoints = 0;
+            List<string> locations = new List<string>();
+            foreach (Match location in markedLocatonsCollection)
+            {
+                string currentLocation = location.Groups["validLocation"].Value;
+                travelPoints += currentLocation.Length;
+                locations.Add(currentLocation);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", locations)}\nTravel Points: {travelPoints}");
+        }
+    }
+}
---
+++ C# Fundamental May 2023/35.REP-AditionalProjects/35.REP-AditionalProjects/02.WorldTour/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.WorldTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string travelingLocations = Console.ReadLine();
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+                string[] commandTokens = command.Split(":");
+                string currentCommand = commandTokens[0];
+                switch (currentCommand)
+                {
+                    case "Add Stop":
+                        int insertIndex = int.Parse(commandTokens[1]);
+                        string valueToInsert = commandTokens[2];
+                        if (insertIndex >= 0 && insertIndex < travelingLocations.Length)
+                        {
+                            travelingLocations = travelingLocations.Insert(insertIndex, valueToInsert);
+                        }
+                        Console.WriteLine(travelingLocations);
+
+                        break;
+                    case "Remove Stop":
+                        int startIndex = int.Parse(commandTokens[1]);
+                        int endIndex = int.Parse(commandTokens[2]);
+                        if (startIndex >= 0 && startIndex < travelingLocations.Length && endIndex >= startIndex && endIndex < travelingLocations.Length)
+                        {
+                            travelingLocations = travelingLocations.Remove(startIndex, endIndex - startIndex + 1);
+                        }
+                        Console.WriteLine(travelingLocations);
+
+                        break;
+                    case "Switch":
+                        string oldStringValue = commandTokens[1];
+                        string newStringValue = commandTokens[2];
+                        travelingLocations = travelingLocations.Replace(oldStringValue, newStringValue);
+                        Console.WriteLine(travelingLocations);
+                        break;
+                }
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {travelingLocations}");
+        }
+    }
+}

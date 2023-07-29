a9e244685ebf5616a50296dc770f0d83aae1f9fd
Miroslav Pavlov <dreb4oz@abv.bg>
2023-07-29 16:25:42 +0300

    Regular Exam Preparation
    added new projects to REP

---
+++ C# Fundamental May 2023/35.REP-AditionalProjects/35.REP-AditionalProjects/01.ActivationKeys/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _01.ActivationKeys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "Generate")
+            {
+                string[] commandTokens = command
+                    .Split(">>>");
+                string currentCommand = commandTokens[0];
+                switch (currentCommand)
+                {
+                    case "Contains":
+                        string substring = commandTokens[1];
+                        IsSubstringExist(activationKey, substring);
+                        break;
+                    case "Flip":
+                        string lowerOrUpper = commandTokens[1];
+                        int startIndexFlip = int.Parse(commandTokens[2]);
+                        int endIndexFlip = int.Parse(commandTokens[3]);   //the range is between startIndex and endIndex(exclusively), in other words length will be (endIndex - startIndex)
+                        activationKey = LettersConversion(activationKey, lowerOrUpper, startIndexFlip, endIndexFlip);
+                        break;
+                    case "Slice":
+                        int startIndexSlice = int.Parse(commandTokens[1]);
+                        int endIndexSlice = int.Parse(commandTokens[2]);  //the range is between startIndex and endIndex(exclusively)
+                        activationKey = LetterRemove(activationKey, startIndexSlice, endIndexSlice);
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Your activation key is: {activationKey}");
+        }
+
+        private static string LetterRemove(string activationKey, int startIndexSlice, int endIndexSlice)
+        {
+            activationKey = activationKey.Remove(startIndexSlice, endIndexSlice - startIndexSlice);
+            Console.WriteLine(activationKey);
+            return activationKey;
+        }
+
+        private static string LettersConversion(string activationKey, string lowerOrUpper, int startIndex, int endIndex)
+        {
+            if (lowerOrUpper == "Upper")
+            {
+                string temp = activationKey.Substring(startIndex, endIndex - startIndex).ToUpper();
+                activationKey = activationKey.Replace(activationKey.Substring(startIndex, endIndex - startIndex), temp);
+                Console.WriteLine(activationKey);
+                return activationKey;
+            }
+
+            else
+            {
+                string temp = activationKey.Substring(startIndex, endIndex - startIndex).ToLower();
+                activationKey = activationKey.Replace(activationKey.Substring(startIndex, endIndex - startIndex), temp);
+                Console.WriteLine(activationKey);
+                return activationKey;
+            }
+        }
+
+        private static void IsSubstringExist(string activationKey, string substring)
+        {
+            if (activationKey.Contains(substring))
+            {
+                Console.WriteLine($"{activationKey} contains {substring}");
+            }
+
+            else
+            {
+                Console.WriteLine("Substring not found!");
+            }
+        }
+    }
+}
---
+++ C# Fundamental May 2023/35.REP-AditionalProjects/35.REP-AditionalProjects/02.EmojiDetector/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _02.EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string validEmojis = @"([:]{2}|[*]{2})(?<emoji>[A-Z][a-z]{2,})\1";
+            string allDigits = @"\d";
+            BigInteger coolTreshhold = 1;
+            foreach (Match match in Regex.Matches(input,allDigits))
+            
+            {
+                coolTreshhold *= BigInteger.Parse(match.Value);
+            }
+
+            if (Regex.Matches(input, allDigits).Count == 0)
+            {
+                coolTreshhold = 0;
+            }
+
+            int emojiCount = Regex.Matches(input, validEmojis).Count;
+            List<string> coolEmojis = new List<string>();
+            foreach (Match match in Regex.Matches(input, validEmojis))
+            {
+                BigInteger currentEmojiCoolnes = 0;
+                foreach (char character in match.Groups["emoji"].Value)
+                {
+                    currentEmojiCoolnes += character;
+                }
+
+                if (currentEmojiCoolnes >= coolTreshhold)
+                {
+                    coolEmojis.Add(match.Value);
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {coolTreshhold}");
+            Console.WriteLine($"{emojiCount} emojis found in the text. The cool ones are:");
+            foreach (var item in coolEmojis)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
